a86b874bb866dd02e42a75c6493331820a6ced53
Vitalii Hutsalo <jacklounter07@gmail.com>
2025-03-03 17:58:10 +0200

    corrected solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.scss';
 
 import todosFromServer from './api/todos.json';
 import usersFromServer from './api/users.json';
+
 import { TodoList } from './components/TodoList/TodoList';
 
 function getUserById(userId) {
